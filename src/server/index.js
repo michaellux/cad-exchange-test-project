f96647f90c5928f6d8dbfbf529bde1f9f27d097f
michaellux <michaellux@yandex.ua>
2021-01-15 00:23:21 +0300

    create server

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
-const os = require('os');
+const formidableMiddleware = require('express-formidable');
 
 const app = express();
+app.use(formidableMiddleware());
+const port = process.env.PORT || 8080;
+const cors = require('cors');
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use(cors());
+app.listen(port, () => console.log(`Backend server live on ${port}`));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.post('/coneParams', (req, res) => {
+  const { coneHeight, numberOfsegments, radius } = req.fields.coneParams;
+  let triangleCoords = [];
+  const pointA = [0, 0, coneHeight];
+  for (let i = 0; i <= numberOfsegments - 1; i += 1) {
+    const pointPiXcoord = radius * Math.cos(2 * Math.PI * i / numberOfsegments);
+    const pointPiYcoord = radius * Math.sin(2 * Math.PI * i / numberOfsegments);
+    const pointPiPlusOneXcoord = radius * Math.cos(2 * Math.PI * (i + 1) / numberOfsegments);
+    const pointPiPlusOneYcoord = radius * Math.sin(2 * Math.PI * (i + 1) / numberOfsegments);
+    const pointPiZcoord = 0;
+    const conicalSurfaceCoords = {
+      A: pointA,
+      Pi: [pointPiXcoord, pointPiYcoord, pointPiZcoord],
+      PiPlusOne: [pointPiPlusOneXcoord, pointPiPlusOneYcoord, pointPiZcoord]
+    };
+    const baseCoords = {
+      Zero: [0, 0, 0],
+      Pi: [pointPiXcoord, pointPiYcoord, pointPiZcoord],
+      PiPlusOne: [pointPiPlusOneXcoord, pointPiPlusOneYcoord, pointPiZcoord]
+    };
+
+    triangleCoords = [...triangleCoords,
+      { conicalSurface: conicalSurfaceCoords, base: baseCoords }];
+  }
+
+  res.send({ triangleCoords });
+});
