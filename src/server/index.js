59cd38da1fe3bfd472640cfff361dd87a40e1b54
michaellux <michaellux@yandex.ua>
2021-01-15 01:10:46 +0300

    connect tool for fix work production mode

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const formidableMiddleware = require('express-formidable');
 
 const app = express();
+app.use(express.static('dist'));
 app.use(formidableMiddleware());
 const port = process.env.PORT || 8080;
 const cors = require('cors');
